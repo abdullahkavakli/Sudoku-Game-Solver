4609df5b0fbbd96b215371cb8185f3e958a17d10
Abdullah KAVAKLI <72142708+abdullahkavakli@users.noreply.github.com>
2022-05-29 23:10:03 +0300

    cleaned

--- sudokusolver.py
+++ sudokusolver.py
@@ -1,3 +1,4 @@
+global GRID
 GRID = [
         [7, 8, 0, 4, 0, 0, 1, 2, 0],
         [6, 0, 0, 0, 7, 5, 0, 0, 9],
@@ -9,78 +10,53 @@ GRID = [
         [1, 2, 0, 0, 0, 7, 4, 0, 0],
         [0, 4, 9, 2, 0, 6, 0, 0, 7]
     ]
-
-
-def sayi_icin_yer_münasip_mi(koordinat,sayımız):
-
-    ith_blok=koordinat[0]//3
-    jth_blok=koordinat[1]//3
-
-    column = [];        # empty list
-    for row in GRID:
-        column.append(row[koordinat[1]])
-
-    if GRID[koordinat[0]].count(sayımız)==0 and column.count(sayımız)==0 and (blokta_o_sayı_var_mı(ith_blok,jth_blok,sayımız)==False): 
-        return True
-    else:
-        return False
-
-    
-def blokta_o_sayı_var_mı(ith_blok,jth_blok,sayımız):
-
-    ith_blok_başlangıcı=ith_blok*3
-    jth_blok_başlangıcı=jth_blok*3 
-
-    flag=0
-    for satır in range(ith_blok_başlangıcı,ith_blok_başlangıcı+3):
-        for sütun in range(jth_blok_başlangıcı,jth_blok_başlangıcı+3):
-            if GRID[satır][sütun]==sayımız:
-                flag = 1
-                break
-    
-    if flag==1:
-        return True
-
-    return False
-
-
-def ilk_bos_yeri_bul():
-
+def suitable_for_number(coordinate,number_to_try):
+    #zero and one comes from coordinate
+    ith_block=coordinate[0]//3
+    jth_block=coordinate[1]//3
+    column = []; 
+    for row in GRID: # transpose of the grid
+        column.append(row[coordinate[1]])
+    # Control for number existence in row, column and block
+    return GRID[coordinate[0]].count(number_to_try)==0 and column.count(number_to_try)==0 and exist_in_block(ith_block,jth_block,number_to_try)==False  
+def exist_in_block(ith_blok,jth_blok,number_to_try):
+    ith_block_start=ith_blok*3
+    jth_block_start=jth_blok*3 
+    exist_flag=0
+    for row in range(ith_block_start,ith_block_start+3):
+        for column in range(jth_block_start,jth_block_start+3):
+            if GRID[row][column]==number_to_try:
+                exist_flag = 1
+                break    
+    return bool(exist_flag)    
+def find_first_empty_place():
     for i in range(9):
         for j in range(9):
             if GRID[i][j]==0:
                 return [i,j]
-
-
-
-def o_sayıyı_yerine_koy(koordinat,sayi):
-    GRID[koordinat[0]][koordinat[1]]=sayi
+def place_number(coordinate,chosen_number):
+    GRID[coordinate[0]][coordinate[1]]=chosen_number
     return True
 
 
 def sudoku_solver():    
-    denencek_koordinat=ilk_bos_yeri_bul()
-    if denencek_koordinat==None:
+    coordinate_to_try=find_first_empty_place()
+    if coordinate_to_try==None:
         return True
-    for sayi in range(1,10):
-        if sayi_icin_yer_münasip_mi(denencek_koordinat,sayi)==True:
-            o_sayıyı_yerine_koy(denencek_koordinat,sayi)
+    for number_to_try in range(1,10):
+        if suitable_for_number(coordinate_to_try,number_to_try)==True:
+            place_number(coordinate_to_try,number_to_try)
             if sudoku_solver()==True:                    
-                return True            
-            #else:
-        GRID[denencek_koordinat[0]][denencek_koordinat[1]]=0           
+                return True  
+        GRID[coordinate_to_try[0]][coordinate_to_try[1]]=0           
     return False
 
 
-def yazdır():
+def print_grid():
     for i in GRID:        
         print (i)
     print("\n")
     return True
-
-#yazdır()
-#sudoku_solver()
-#yazdır()
 
 
 def take_the_board_and_solve(new_board):
